38b46daf81beab0220d4ad0ec321017697fa8d06
Ravikumar K Solaimalai <ksravikumar2005@gmail.com>
2018-11-18 14:17:55 +0000

    Initial Commit

---
+++ cluster-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+/usr/local/bin/docker-entrypoint.sh rabbitmq-server -detached
+rabbitmqctl stop_app
+rabbitmqctl join_cluster rabbit@rabbitmq1
+rabbitmqctl stop
+
+sleep 3s
+
+rabbitmq-server
+
